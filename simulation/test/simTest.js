1de33be9fce7b8b16619d983850f5da264ffacf0
hhummel <harry.hummel@eventhorizoncapital.com>
2017-07-01 16:42:30 -0400

    Adding test for prices

--- simulation/test/simTest.js
+++ simulation/test/simTest.js
@@ -116,6 +116,7 @@ describe("Market Simulation", function() {
       const s = new Stock('S', 1000, 15.0, 0.5, [10.0, 11.0, 12.0]);
       const p = new Stock('P', 3000, 10.0, 2.0, [25.0, 21.0, 18.0]);
       const q = new Stock('Q', 4000, 5.0, 10.0, [30.0, 31.0, 32.0]);
+      const r = new Stock('R', 5000, 15.0, 5.0, [30.0, 25.0, 20.0]);
       const universe = new Map([[s.ticker, s], [p.ticker, p], [q.ticker, q]]);
       const t = new Trader('Tom', [0.1,2,-1], [['S', 100], ['P', 200]], 500.00, universe); 
       const j = new Trader('Joe', [0.2,1,-2], [['Q', 100], ['P', 200]], 1500.00, universe); 
@@ -153,18 +154,29 @@ describe("Market Simulation", function() {
         ["Huck", "Q", 29.924999999999997, -33.333333333333336]
       ]);
     });
+
+    it("updates prices", function(){
+      const s = new Stock('S', 1000, 15.0, 0.5, [10.0, 11.0, 12.0]);
+      const p = new Stock('P', 3000, 10.0, 2.0, [25.0, 21.0, 18.0]);
+      const q = new Stock('Q', 4000, 5.0, 10.0, [30.0, 31.0, 32.0]);
+      const r = new Stock('R', 5000, 15.0, 3.0, [30.0, 25.0, 20.0]);
+      const universe = new Map([[s.ticker, s], [p.ticker, p], [q.ticker, q], [r.ticker, r]]);
+      const t = new Trader('Tom', [0.1,2,-1], [['S', 100], ['P', 200]], 500.00, universe); 
+      const j = new Trader('Joe', [0.2,1,-2], [['Q', 100], ['P', 200]], 1500.00, universe); 
+      const a = new Trader('Huck', [0.3,0.5,-5], [['Q', 100], ['S', 200]], 1500.00, universe); 
+      const traders = new Map([[t.name, t], [j.name, j], [a.name, a]]);
+      const e = new Exchange(universe, traders);
+      const book = e.getOrderBook();
+      const trades = e.getTrades(book);
+      e.updatePrices(trades); 
+      expect(e.universe.get('S').price).deep.equal([10.075, 10.0, 11.0, 12.0]);
+      expect(e.universe.get('P').price).deep.equal([25.075000000000003, 25.0, 21.0, 18.0]);
+      expect(e.universe.get('Q').price).deep.equal([29.924999999999997, 30.0, 31.0, 32.0]);
+      expect(e.universe.get('R').price).deep.equal([30.0, 30.0, 25.0, 20.0]);
+    });
   });
 });
 
-[["Joe", "S", 10.075, 50], 
- ["Huck", "S", 10.075, 50],
- ["Tom", "S", 10.075, -100],
- ["Huck", "P", 25.075000000000003, 30],
- ["Tom", "P", 25.075000000000003, -15],
- ["Joe", "P", 25.075000000000003, -15],
- ["Tom", "Q", 29.924999999999997, 8.333333333333334],
- ["Joe", "Q", 29.924999999999997, 25],
- ["Huck", "Q", 29.924999999999997, -33.333333333333336]
-]
+
 
 
