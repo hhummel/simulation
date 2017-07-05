3ae809b08d15fdeb920540d342c51359d54dc86d
hhummel <harry.hummel@eventhorizoncapital.com>
2017-07-05 19:56:31 -0400

    Increasing traders in simulation

--- simulation/src/sim.js
+++ simulation/src/sim.js
@@ -14,17 +14,30 @@ const universe = new Map([[s.ticker, s], [p.ticker, p], [q.ticker, q], [r.ticker
 
 const portfolio = new Map(
   [
-    ['Tom', [[['S', 100], ['P', 200], ['R', 2]]]],
-    ['Dick', [[['P', 100], ['Q', 200], ['R', 2]]]],
-    ['Harry', [[['S', 100], ['Q', 200], ['R', 2]]]]
+    ['Tom', [[['S', 100], ['P', 200], ['R', 25]]]],
+    ['Dick', [[['P', 100], ['Q', 200], ['R', 25]]]],
+    ['Harry', [[['S', 100], ['Q', 200], ['R', 25]]]],
+    ['Curly', [[['S', 100], ['P', 200], ['R', 25]]]],
+    ['Moe', [[['P', 100], ['Q', 200], ['R', 25]]]],
+    ['Larry', [[['S', 100], ['Q', 200], ['R', 25]]]],
+    ['Groucho', [[['S', 100], ['P', 200], ['R', 25]]]],
+    ['Harpo', [[['P', 100], ['Q', 200], ['R', 25]]]],
+    ['Zeppo', [[['S', 100], ['Q', 200], ['R', 25]]]],
   ]
+
 );
 
 const cash = new Map(
   [
     ['Tom', [500.00]],
     ['Dick', [1000.00]],
-    ['Harry', [1500.00]]
+    ['Harry', [1500.00]],
+    ['Curly', [500.00]],
+    ['Moe', [1000.00]],
+    ['Larry', [1500.00]],
+    ['Groucho', [500.00]],
+    ['Harpo', [1000.00]],
+    ['Zeppo', [1500.00]],
   ]
 );
 
@@ -39,17 +52,32 @@ while (true) {
   const t = new Trader('Tom', [0.1, 3, -2], portfolio.get('Tom')[0], cash.get('Tom')[0], universe); 
   const u = new Trader('Dick', [0.2, 2, -3], portfolio.get('Dick')[0], cash.get('Dick')[0], universe); 
   const v = new Trader('Harry', [0.3, 1, -1], portfolio.get('Harry')[0], cash.get('Harry')[0], universe); 
-  const traders = new Map([[t.name, t], [u.name, u], [v.name, v]]);
+  const c = new Trader('Curly', [0.2, 6, -4], portfolio.get('Curly')[0], cash.get('Curly')[0], universe); 
+  const m = new Trader('Moe', [0.4, 4, -6], portfolio.get('Moe')[0], cash.get('Moe')[0], universe); 
+  const l = new Trader('Larry', [0.6, 2, -2], portfolio.get('Larry')[0], cash.get('Larry')[0], universe); 
+  const g = new Trader('Groucho', [0.05, 1.5, -1], portfolio.get('Groucho')[0], cash.get('Groucho')[0], universe); 
+  const h = new Trader('Harpo', [0.1, 1, -1.5], portfolio.get('Harpo')[0], cash.get('Harpo')[0], universe); 
+  const z = new Trader('Zeppo', [0.15, 0.5, -0.5], portfolio.get('Zeppo')[0], cash.get('Zeppo')[0], universe); 
+  const traders = new Map(
+    [
+      [t.name, t], 
+      [u.name, u], 
+      [v.name, v],
+      [c.name, c], 
+      [m.name, m], 
+      [l.name, l],
+      [g.name, g], 
+      [h.name, h], 
+      [z.name, z],
+    ]);
 
   //Do trades
   const exchange = new Exchange(universe, traders);
   const book = exchange.getOrderBook();
   const trades = exchange.getTrades(book);
-  //console.log(book.get('S'));
-  //console.log(book.get('P'));
-  //console.log(book.get('Q'));
-  //console.log(book.get('R'));
   if (trades.length === undefined || cycle === limit) {
+
+    //Output cumulative result
     console.log("Stocks: ");
     universe.forEach((stock, ticker) => console.log(ticker, stock.price));
     console.log("Portfolios: ");
@@ -64,7 +92,7 @@ while (true) {
   portfolio.forEach((array, name) => array.unshift(Array.from(newPortfolio.get(name))));
   cash.forEach((array, name) => array.unshift(newCash.get(name)));
 
-  //Output results
+  //Output cycle results
   console.log("Cycle: ", cycle);
   console.log("Trades: ", trades);
 }
